08d6e85abd2eaa1d0c601a9e3ec88cc7cde09e79
Rohan Kumar <rohankumar@ucsb.edu>
2022-06-23 12:02:27 -0700

    Initial Commit

---
+++ Age Predictor/Age_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Age_PredictorApp.swift
+//  Age Predictor
+//
+//  Created by Rohan Kumar on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Age_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
