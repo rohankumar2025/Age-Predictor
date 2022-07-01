0d4816591e355f0477fac3be66a58e51c28b8780
Rohan Kumar <rohankumar@ucsb.edu>
2022-07-01 12:16:41 -0700

    added non maximum suppression

--- Age Predictor/ContentView.swift
+++ Age Predictor/ContentView.swift
@@ -104,16 +104,16 @@ struct ContentView: View {
     }
     
     
-    func detectObjsInImage(image: UIImage) /* -> [(UIImage, (Int, Int, Int, Int), String)]  */ {
+    func detectObjsInImage(image: UIImage) {
         // initialize constants used for the object detection procedure
-        let PYR_SCALE = 2.0 // Scale factor used in imagePyramid() function (Higher Value = faster, less accurate) (Lower value = slower, more accurate)
-        let WIN_STEP = 20 // Size of step that the Sliding Window is taking
-        let ROI_SIZE = (200, 200) // Dimensions of Sliding Window (Should be close in size to the object trying to be found)
+        let PYR_SCALE = 1.75 // Scale factor used in imagePyramid() function (Higher Value = faster, less accurate)
+        let WIN_STEP = 25 // Size of step that the Sliding Window is taking
+        let ROI_SIZE = (200, 200) // Dimensions of Sliding Window (Should be close in size to the object/face)
         let uploadURL="https://askai.aiclub.world/018cb7b9-ad1c-4c60-84a1-267fac249865" // AI URL
         let MIN_CONFIDENCE_SCORE = 0.90 // Threshold value for rectangle to be drawn
         let INPUT_SIZE = (image.size.width, image.size.height) // Dimensions of Original Image
 
-        var arrayOut:[(UIImage, (Int, Int, Int, Int), String)] = [] // Array containing [ ( ROI_image, (X+Y Coordinates for rectangle), prediction ) ]
+        var arrayOut:[(UIImage, (Int, Int, Int, Int), String, Double)] = [] // Array containing [ ( ROI_image, (X+Y Coordinates for rectangle), prediction, Confidence_Score ) ]
         let pyramid = imagePyramid(image: image, scale: PYR_SCALE)
         
         let apiCall = DispatchGroup() // Creates DispatchGroup for apiCall
@@ -152,7 +152,7 @@ struct ContentView: View {
                         
                         // Appends Data to arrayOut if ROI has more than minimum confidence score
                         if confidenceScore >= MIN_CONFIDENCE_SCORE {
-                            arrayOut.append( (roiOrig, (I, J, I+w, J+h), prediction) )
+                            arrayOut.append( (roiOrig, (I, J, I+w, J+h), prediction, confidenceScore) )
                         }
                     case .failure:
                         print("Failure")
@@ -164,15 +164,76 @@ struct ContentView: View {
         
         
         apiCall.notify(queue: .main, execute: {
-            drawRectangleOnImage(arrayOut) // Calls drawRectangleOnImage function when all API calls are completed
+            drawRectangleOnImage( nonMaximumSuppression(arrayOut) ) // Calls drawRectangleOnImage function when all API calls are completed
         })
     }
+    
+    func nonMaximumSuppression(_ infoArray:[(UIImage, (Int, Int, Int, Int), String, Double)]) -> [((Int, Int, Int, Int), String)]{
+        let ROI_SIZE = (200, 200) // Dimensions of Sliding Window (Should be close in size to the object/face)
+        var arraySortedByOverlap:[ [((Int, Int, Int, Int), String, Double)] ] = [] // Array of Arrays containing groups of overlapping rectangles
+        // Each index of arrayOut contains an array of overlapping rectangles
+        
+        // Iterates through all entries in infoArray
+        for (_, (x1,y1,x2,y2), prediction, confidenceScore) in infoArray {
+            var overlapsOtherRectangle = false // Boolean Variable used to determine what to do with Current info
+            // Appends current info to arrayOut if overlapsOtherRectangle remains false
+            // Averages value with overlapping rectangle if overlapsOtherRectangle is true
+            
+            // Iterates through all current entries in arrayOut if arrayOut is not empty
+            if !(arraySortedByOverlap.isEmpty) {
+                for i in 0...arraySortedByOverlap.count-1 {
+                    if isOverlapping(rect1: (x1,y1,x2,y2), rectArray: arraySortedByOverlap[i]) {
+                        // Adds to current subArray if overlaps
+                        arraySortedByOverlap[i].append(((x1,y1,x2,y2), prediction, confidenceScore))
+                        overlapsOtherRectangle = true
+                        break
+                    } // END IF STATEMENT
+                } // END INNER FOR LOOP
+            } // END IF STATEMENT
+            
+            // Appends current info if it does not overlap with any other rectangle
+            if !overlapsOtherRectangle {
+                let tempArray = [((x1,y1,x2,y2), prediction, confidenceScore)]
+                arraySortedByOverlap.append(tempArray)
+            }
+        }
+        
+        var arrayOut:[((Int, Int, Int, Int), String)] = []
+        
+        // Iterates through all subArrays created previously
+        for subArray in arraySortedByOverlap {
+            var maxConfidenceScore:Double = 0
+            var rectOfMaxScore = (0,0,0,0)
+            // Iterates through each subArray to find maximum confidenceScore
+            for (currRect, _, confidenceScore) in subArray {
+                if confidenceScore > maxConfidenceScore {
+                    maxConfidenceScore = confidenceScore
+                    rectOfMaxScore = currRect
+                }
+            }
+            arrayOut.append( (rectOfMaxScore, subArray[0].1) )
+        }
+        
+        print(arrayOut)
+        return arrayOut
+    }
+    
+    // Helper function to check if Rect1 overlaps with any rectangle in rectArray
+    func isOverlapping(rect1: (Int,Int,Int,Int), rectArray: [((Int,Int,Int,Int), String, Double)]) -> Bool {
+        // Returns False if Rectangles are not overlapping
+        for rect2 in rectArray {
+            if !( (rect1.2 < rect2.0.0) || (rect2.0.2 < rect1.0) || (rect1.3 < rect2.0.1) || (rect2.0.3 < rect1.1) ) {
+                return true
+            }
+        }
+        return false // Returns false if no collisions are detected
+    }
 
-    func drawRectangleOnImage(_ infoArray:[(UIImage, (Int, Int, Int, Int), String)]) {
+    func drawRectangleOnImage(_ arrayIn:[((Int, Int, Int, Int), String)]) {
         let imageSize = inputImage!.size
         var editedInputImage:UIImage = inputImage!
-        
-        for (_, (x1,y1,x2,y2), _) in infoArray {
+                
+        for ((x1,y1,x2,y2), _) in arrayIn {
             let scale: CGFloat = 0
             UIGraphicsBeginImageContextWithOptions(imageSize, false, scale) // Begins Drawing
             editedInputImage.draw(at: CGPoint.zero) // Sets Starting Point at (0,0)
@@ -190,27 +251,27 @@ struct ContentView: View {
         inputImage = editedInputImage // Replaces inputImage with inputImage with Rectangles
         self.loadingText = "" // Resets Loading Text
     }
+    
+    func convertToAgeGroup(_ prediction:String) -> String {
+        switch prediction {
+        case "Kid":
+            return "6-20"
+        case "Young Adult":
+            return "21-35"
+        case "Adult":
+            return "36-59"
+        case "Elderly":
+            return "60+"
+        default:
+            return "Invalid Photo"
+        }
+    }
 
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-func convertToAgeGroup(_ prediction:String) -> String {
-    switch prediction {
-    case "Kid":
-        return "6-20"
-    case "Young Adult":
-        return "21-35"
-    case "Adult":
-        return "36-59"
-    case "Elderly":
-        return "60+"
-    default:
-        return "Invalid Photo"
     }
 }
 
