6146bd58c91e3005770a5651185b1bb06d45c981
Rohan Kumar <rohankumar@ucsb.edu>
2022-07-06 10:57:28 -0700

    added api call function

--- Age Predictor/ContentView.swift
+++ Age Predictor/ContentView.swift
@@ -52,7 +52,7 @@ struct ContentView: View {
                     }
                     
                     Button("How Old Am I?"){ // SUBMIT BUTTON
-                        self.buttonPressed()
+                        self.showingImagePicker = true
                     }
                     .padding(.all, 14.0)
                     .background(UIPink)
@@ -67,53 +67,74 @@ struct ContentView: View {
         }
     }
     
-    func buttonPressed() {
-        self.showingImagePicker = true
-    }
-    
     func processImage() {
         self.showingImagePicker = false // Removes Camera Roll Image picker from UI
         self.predictedAgeGroup="..." // Temporary Message while AI is loading
         guard let inputImage = inputImage else {return} // Unwraps inputImage
-        // Compresses and Converts image to correct output type
-        let imageJPG=inputImage.jpegData(compressionQuality: 0.0034)!
-        let imageB64 = Data(imageJPG).base64EncodedData()
-        // Sends image to AI URL
-        let uploadURL="https://askai.aiclub.world/018cb7b9-ad1c-4c60-84a1-267fac249865"
-        AF.upload(imageB64, to: uploadURL).responseJSON { response in
-            // Receives output from AI
-            switch response.result {
-            case .success(let responseJsonStr):
-                let myJson = JSON(responseJsonStr)
-                let predictedValue = myJson["predicted_label"].string // Parses Result for predicted value
-                let confidence = Array(myJson["score"]) // Parses Result for all 4 confidence scores
-                
-                // Saves highest Confidence Score (corresponds to score of predicted label)
-                var maxConfidenceScore:Double = 0
-                for (_, c2) in confidence {
-                    if ((c2.rawValue as! Double) > maxConfidenceScore) { maxConfidenceScore = c2.rawValue as! Double}
-                }
 
-                self.predictedAgeGroup = convertToAgeGroup(predictedValue!) // returns result, which is displayed on UI
-            case .failure(let error):
-                print("\n\n Request failed with error: \(error)")
-            }
-        }
+        // Processes API Call on whole image
+        processAPICall(image: inputImage, {(prediction, _) in
+            // Displays prediction to UI
+            self.predictedAgeGroup = convertToAgeGroup(prediction)
+        })
+        
         self.loadingText = "Detecting Objects..."
+        // Calls Object Detection Function
         detectObjsInImage(image: inputImage)
     }
     
-    
+    func processAPICall(image: UIImage, _
+        completion: @escaping (_ prediction:String, _ confidenceScore:Double) -> Void) {
+        let uploadURL="https://askai.aiclub.world/018cb7b9-ad1c-4c60-84a1-267fac249865" // AI URL
+        let apiCall = DispatchGroup()
+        
+        var prediction = ""
+        var confidenceScore = 0.0
+        // Pre processing before image is sent to AI
+        let imageJPG = image.jpegData(compressionQuality: 0.0034)!
+        let imageB64 = Data(imageJPG).base64EncodedData()
+        
+        // Enters Dispatch Group before starting API Call
+        apiCall.enter()
+        
+        AF.upload(imageB64, to: uploadURL).responseJSON { response in
+            switch response.result {
+            case .success(let responseJsonStr):
+                let myJson = JSON(responseJsonStr) // Converts response into JSON Format
+                if let pred = myJson["predicted_label"].string {
+                    prediction = pred // Parses response to find prediction
+                }
+                
+                // Calculates confidence score of prediction
+                let confidence = Array(myJson["score"])
+                // Sets confidenceScore to highest confidence among the 4 categories outputted by the AI
+                for (_, c2) in confidence {
+                    if ((c2.rawValue as! Double) > confidenceScore) { confidenceScore = c2.rawValue as! Double}
+                }
+            case .failure:
+                print("Failure")
+            } // END SWITCH-CASE STATEMENT
+            
+            // Leaves Dispatch Group after finishing API call
+            apiCall.leave()
+        } // END UPLOAD
+            
+        
+        apiCall.notify(queue: .main, execute: {
+            // Calls Completion Handler after API Call finishes
+            completion(prediction, confidenceScore)
+        })
+    }
+
     func detectObjsInImage(image: UIImage) {
         // initialize constants used for the object detection procedure
         let PYR_SCALE = 1.75 // Scale factor used in imagePyramid() function (Higher Value = faster, less accurate)
         let WIN_STEP = 25 // Size of step that the Sliding Window is taking
         let ROI_SIZE = (200, 200) // Dimensions of Sliding Window (Should be close in size to the object/face)
-        let uploadURL="https://askai.aiclub.world/018cb7b9-ad1c-4c60-84a1-267fac249865" // AI URL
         let MIN_CONFIDENCE_SCORE = 0.90 // Threshold value for rectangle to be drawn
         let INPUT_SIZE = (image.size.width, image.size.height) // Dimensions of Original Image
 
-        var arrayOut:[(UIImage, (Int, Int, Int, Int), String, Double)] = [] // Array containing [ ( ROI_image, (X+Y Coordinates for rectangle), prediction, Confidence_Score ) ]
+        var arrayOut:[((Int, Int, Int, Int), Double)] = [] // Array containing [ ( (X+Y Coordinates for rectangle), Confidence_Score ) ]
         let pyramid = imagePyramid(image: image, scale: PYR_SCALE)
         
         let apiCall = DispatchGroup() // Creates DispatchGroup for apiCall
@@ -131,60 +152,49 @@ struct ContentView: View {
                 let w = Int(Double(ROI_SIZE.0) * scale)
                 let h = Int(Double(ROI_SIZE.1) * scale)
                 
-                // Processing roiOrig before sending it into AI for processing
-                let roiJPG=roiOrig.jpegData(compressionQuality: 0.0034)!
-                let roiB64 = Data(roiJPG).base64EncodedData()
-                
                 apiCall.enter() // Task Enters Dispatch Group before API Call Begins
                 // Sends processed image to AI
-                AF.upload(roiB64, to: uploadURL).responseJSON { response in
-                    switch response.result {
-                    case .success(let responseJsonStr):
-                        let myJson = JSON(responseJsonStr) // Converts response into JSON Format
-                        let prediction = myJson["predicted_label"].string! // Parses response to find prediction
-                        
-                        // Calculates confidence score of prediction
-                        let confidence = Array(myJson["score"])
-                        var confidenceScore:Double = 0
-                        for (_, c2) in confidence {
-                            if ((c2.rawValue as! Double) > confidenceScore) { confidenceScore = c2.rawValue as! Double}
-                        }
-                        
-                        // Appends Data to arrayOut if ROI has more than minimum confidence score
-                        if confidenceScore >= MIN_CONFIDENCE_SCORE {
-                            arrayOut.append( (roiOrig, (I, J, I+w, J+h), prediction, confidenceScore) )
-                        }
-                    case .failure:
-                        print("Failure")
-                    } // END SWITCH-CASE STATEMENT
+                processAPICall(image: roiOrig, {(_, confidenceScore) in
+                    // Appends Data to arrayOut if ROI has more than minimum confidence score
+                    if confidenceScore >= MIN_CONFIDENCE_SCORE {
+                        arrayOut.append( ((I, J, I+w, J+h), confidenceScore) )
+                    }
                     apiCall.leave() // Task Leaves Dispatch Group after API call is completed
-                } // END UPLOAD
+                    
+                }) // END API CALL
+                       
             } // END INNER FOR LOOP
         } // END OUTER FOR LOOP
         
-        
         apiCall.notify(queue: .main, execute: {
-            drawRectangleOnImage( nonMaximumSuppression(arrayOut) ) // Calls drawRectangleOnImage function when all API calls are completed
+            // (1) Lets all API calls complete
+            // (2) Calls nonMaximumSuppression function to reduce overlapping rectangles
+            // (3) Draws remaining rectangles on the image
+            drawRectangleOnImage( nonMaximumSuppression(arrayOut) )
         })
     }
     
-    func nonMaximumSuppression(_ infoArray:[(UIImage, (Int, Int, Int, Int), String, Double)]) -> [((Int, Int, Int, Int), String)]{
-        let ROI_SIZE = (200, 200) // Dimensions of Sliding Window (Should be close in size to the object/face)
-        var arraySortedByOverlap:[ [((Int, Int, Int, Int), String, Double)] ] = [] // Array of Arrays containing groups of overlapping rectangles
+    func nonMaximumSuppression(_ infoArray:[((Int, Int, Int, Int), Double)]) -> [(Int, Int, Int, Int)]{
+        var arrayNoOverlaps:[ ((Int, Int, Int, Int), Double) ] = [] // Array of Arrays containing groups of overlapping rectangles
         // Each index of arrayOut contains an array of overlapping rectangles
         
         // Iterates through all entries in infoArray
-        for (_, (x1,y1,x2,y2), prediction, confidenceScore) in infoArray {
-            var overlapsOtherRectangle = false // Boolean Variable used to determine what to do with Current info
-            // Appends current info to arrayOut if overlapsOtherRectangle remains false
-            // Averages value with overlapping rectangle if overlapsOtherRectangle is true
+        for ((x1,y1,x2,y2), confidenceScore) in infoArray {
+            var overlapsOtherRectangle = false
             
-            // Iterates through all current entries in arrayOut if arrayOut is not empty
-            if !(arraySortedByOverlap.isEmpty) {
-                for i in 0...arraySortedByOverlap.count-1 {
-                    if isOverlapping(rect1: (x1,y1,x2,y2), rectArray: arraySortedByOverlap[i]) {
-                        // Adds to current subArray if overlaps
-                        arraySortedByOverlap[i].append(((x1,y1,x2,y2), prediction, confidenceScore))
+            // Appends current info to arrayOut if overlapsOtherRectangle remains false
+            // Compares to overlapping rectangle if overlapsOtherRectangle is true
+            // Replaces current overlapping rectangle if confidence score is greater
+            // Else, leaves arrayOut as is
+            
+            // Iterates through all current entries in arrayNoOverlaps if arrayNoOverlaps is not empty
+            if !(arrayNoOverlaps.isEmpty) {
+                for i in 0...arrayNoOverlaps.count-1 {
+                    if isOverlapping(rect1: (x1,y1,x2,y2), rect2: arrayNoOverlaps[i].0) {
+                        // Replaces current rectangle if confidence score is higher
+                        if confidenceScore > arrayNoOverlaps[i].1 {
+                            arrayNoOverlaps[i] = ((x1,y1,x2,y2), confidenceScore)
+                        }
                         overlapsOtherRectangle = true
                         break
                     } // END IF STATEMENT
@@ -193,54 +203,39 @@ struct ContentView: View {
             
             // Appends current info if it does not overlap with any other rectangle
             if !overlapsOtherRectangle {
-                let tempArray = [((x1,y1,x2,y2), prediction, confidenceScore)]
-                arraySortedByOverlap.append(tempArray)
+                arrayNoOverlaps.append(((x1,y1,x2,y2), confidenceScore))
             }
         }
         
-        var arrayOut:[((Int, Int, Int, Int), String)] = []
-        
-        // Iterates through all subArrays created previously
-        for subArray in arraySortedByOverlap {
-            var maxConfidenceScore:Double = 0
-            var rectOfMaxScore = (0,0,0,0)
-            // Iterates through each subArray to find maximum confidenceScore
-            for (currRect, _, confidenceScore) in subArray {
-                if confidenceScore > maxConfidenceScore {
-                    maxConfidenceScore = confidenceScore
-                    rectOfMaxScore = currRect
-                }
-            }
-            arrayOut.append( (rectOfMaxScore, subArray[0].1) )
+        // Returns array of non-overlapping rectangle coordinates
+        var arrayOut:[(Int, Int, Int, Int)] = []
+        for (coordinates, _) in arrayNoOverlaps {
+            arrayOut.append(coordinates)
         }
-        
-        print(arrayOut)
         return arrayOut
     }
     
     // Helper function to check if Rect1 overlaps with any rectangle in rectArray
-    func isOverlapping(rect1: (Int,Int,Int,Int), rectArray: [((Int,Int,Int,Int), String, Double)]) -> Bool {
+    func isOverlapping(rect1: (Int,Int,Int,Int), rect2: (Int,Int,Int,Int)) -> Bool {
         // Returns False if Rectangles are not overlapping
-        for rect2 in rectArray {
-            if !( (rect1.2 < rect2.0.0) || (rect2.0.2 < rect1.0) || (rect1.3 < rect2.0.1) || (rect2.0.3 < rect1.1) ) {
-                return true
-            }
+        if !( (rect1.2 < rect2.2) || (rect2.2 < rect1.0) || (rect1.3 < rect2.1)     || (rect2.3 < rect1.1) ) {
+            return true
         }
         return false // Returns false if no collisions are detected
     }
 
-    func drawRectangleOnImage(_ arrayIn:[((Int, Int, Int, Int), String)]) {
+    func drawRectangleOnImage(_ arrayIn:[(Int, Int, Int, Int)]) {
         let imageSize = inputImage!.size
         var editedInputImage:UIImage = inputImage!
                 
-        for ((x1,y1,x2,y2), _) in arrayIn {
+        for (x1,y1,x2,y2) in arrayIn {
             let scale: CGFloat = 0
             UIGraphicsBeginImageContextWithOptions(imageSize, false, scale) // Begins Drawing
             editedInputImage.draw(at: CGPoint.zero) // Sets Starting Point at (0,0)
             let rectangle = CGRect(x: x1, y: y1, width: x2-x1, height: y2-y1) // Creates Rectangle Object at the correct x and y coordinates
-            // Sets Stroke Color
-            let color:UIColor = .systemPink
+            let color:UIColor = .systemPink // Sets Stroke Color
             color.set()
+            
             // Draws Rectangle "Path" on top of UIImage
             let rect:UIBezierPath = UIBezierPath(rect: rectangle)
             rect.lineWidth = 4.5
